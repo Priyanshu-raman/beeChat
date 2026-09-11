d995a1ab1351ea0b0edd7d568177844892c70e6b
Priyanshu-raman <ramanpriyashu08@gmail.com>
2026-09-11 10:33:50 +0530

    initialize BEECHAT project

---
+++ backend/src/main/java/beechat/BeechatApplication.java
@@ -0,0 +1,12 @@
+package beechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeechatApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/beechat/BeechatApplicationTests.java
@@ -0,0 +1,13 @@
+package beechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
